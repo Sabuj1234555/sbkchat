94b9d646b0e94ac5cd4c190c9a6b7397dd527614
Sabuj1234555 <programarprogram@gmail.com>
2026-07-02 12:14:10 +0600

    Add debug logging to connect method and improve error handling

--- chat/consumes.py
+++ chat/consumes.py
@@ -1,33 +1,43 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync # গ্রুপ সেন্ডের জন্য এটি প্রয়োজন
+from asgiref.sync import async_to_sync # গ্রুপ সেন্ডের জন্য এটি প্রয়োজন
 from .models import Messages, ChatRoom
 from urllib.parse import parse_qs
 from rest_framework.authtoken.models import Token
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        print("CONNECT START")
+
         query = parse_qs(self.scope["query_string"].decode())
         token_key = query.get("token", [None])[0]
+        print(token_key)
+
         try:
             token = Token.objects.get(key=token_key)
+            print("TOKEN OK")
+
             self.user = token.user
-            
-            # গ্রুপের একটি নির্দিষ্ট নাম দেওয়া (যেমন: chat_sbk_developer)
-            self.room_group_name = 'chat_sbk_developer'
-            
-            # ইউজারকে চ্যানেলের গ্রুপে যুক্ত করা
+
+            self.room_group_name = "chat_sbk_developer"
+
             async_to_sync(self.channel_layer.group_add)(
                 self.room_group_name,
                 self.channel_name
             )
-            
+
+            print("GROUP OK")
+
             self.accept()
-        except Token.DoesNotExist:
+
+            print("ACCEPT OK")
+
+        except Exception as e:
+            print("ERROR:", e)
             self.close()
 
     def disconnect(self, close_code):
-        # কানেকশন বন্ধ হলে গ্রুপ থেকে বের করে দেওয়া
+        # কানেকশন বন্ধ হলে গ্রুপ থেকে বের করে দেওয়া
         if hasattr(self, 'room_group_name'):
             async_to_sync(self.channel_layer.group_discard)(
                 self.room_group_name,
@@ -58,7 +68,6 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # গ্রুপ থেকে মেসেজ রিসিভ করে ফ্রন্টএন্ডে পাঠানো
-        # গ্রুপ থেকে মেসেজ রিসিভ করে ফ্রন্টএন্ডে পাঠানো
     def chat_message(self, event):
         # চেক করা: এই মেসেজটি যে ইউজার রিসিভ করছে, সে নিজেই কি প্রেরক?
         is_sender = (self.user.id == event['user_id'])
@@ -70,4 +79,3 @@ class ChatConsumer(WebsocketConsumer):
             "uploaded_at": event['uploaded_at'],
             "is_sender": is_sender # ফ্রন্টএন্ডের জন্য নতুন ফ্ল্যাগ
         }))
-
