9e55a3d1b4a6f4a19f49be88d300ffb95ac65c80
Sabuj1234555 <programarprogram@gmail.com>
2026-07-02 11:14:35 +0600

    Update ALLOWED_HOSTS to include .onrender.com, localhost, and 127.0.0.1

--- sbkchat/settings.py
+++ sbkchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=ti6+1#!5rhq9*ppi9l07xfw!cyb-k0f)(ar5v*_n6%&t99((%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
